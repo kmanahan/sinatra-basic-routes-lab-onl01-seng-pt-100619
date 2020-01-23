97677b581fd5ba25d87dd8f6cb1a6a08e57e1573
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-23 01:31:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do 
+<<<<<<< HEAD
     "My name is __" 
   end
   
@@ -11,5 +12,7 @@ class App < Sinatra::Base
   
   get '/favorite-song' do 
     "My favorite song is __"
+=======
+>>>>>>> 1a475def8ae39ee8410d960543acd3f472f407f0
   end
 end
